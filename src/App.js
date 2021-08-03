6ef8b675f1e4db4736477594418e4fe7b3fe5853
DesiMusfiroh <musfiroh.desi@gmail.com>
2021-08-04 02:09:13 +0700

    auth login and get token

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import './App.css';
-import Login from './components/login';
+import {Login, getToken} from './components/login';
 import Header from './components/header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useEffect, useState} from 'react';
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getToken();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+    }
+  });
+
   return (
     <div className="App">
-      <Header />
-      <Login />
+      {
+        token ? 
+         <h1>{token}</h1> 
+        : ( <Login /> )
+      }
+   
     </div>
   );
 }
--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,48 +1,32 @@
 import React from 'react';
-import SearchBar from '../searchbar';
 import './style.css';
 
-class Login extends React.Component {
+const authEndpoint = "https://accounts.spotify.com/authorize";
+const redirectUri = "http://localhost:3000/";
+const clientId = "917ecd3283244dbc899b99cfb5645f9b";
+const scopes = [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+];
 
-    constructor(props) {
-        super(props)
-        this.scope = 'playlist-modify-private';
-        this.link = `https://accounts.spotify.com/authorize?client_id=85a543ba79e54f91acdda93b9b01f6a8&response_type=token&redirect_uri=http://localhost:3000/`;
-        this.state = {
-           playlist: this.props.data,
-           token: null,
-        }
-    }
+const accessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
 
-    getParams = () => {
-        const hash = window.location.hash.substr(1);
-        const params = new URLSearchParams(hash);
-        return params;
-    }
-
-    componentDidMount() {
-        const params = this.getParams();
-        const token = params.get('access_token');
-
-        if (token) {
-            this.setState({
-                token
-            })
-        }
-    }
-
-    render() {
-        const {token} = this.state;
-        return (
-            <div id="login">
-                { 
-                    this.state.token === null 
-                    ? <a href={this.link} onClick={this.handleLogin}>Login</a>
-                    : <SearchBar token={token}/>
-                } 
-            </div>
-        );
-    }
+export function Login() {
+    return (
+        <div className="login">
+            <img src="https://www.freepnglogos.com/uploads/spotify-logo-png/spotify-icon-green-logo-8.png" alt="spotify-logo"/>
+            <a href={accessUrl} >LOGIN</a>
+        </div>
+    )
 }
 
-export default Login;
+export const getToken = () => {
+    return window.location.hash.substring(1).split('&').reduce((initial, item) => {
+        let parts = item.split('=');
+        initial[parts[0]] = decodeURIComponent(parts[1]);
+        return initial;
+    }, {});
+}
